7bc005910291eb833b4d676df78aa03a573ecc8a
htwright <harrisontwright@gmail.com>
2017-05-02 15:17:46 -0500

    going to heroku

--- routes/blogPostRoutes.js
+++ routes/blogPostRoutes.js
@@ -7,6 +7,8 @@ const jsonParser = bodyParser.json();
 const {Post} = require('../models');
 
 
+
+
 // const {
 //     BlogPosts
 // } = require('../models.js');
@@ -22,45 +24,70 @@ router.get('/', (req, res)=> {
             <div style="background:linear-gradient(to bottom, rgba(30,87,153,0) 0%,rgba(55,145,192,1) 100%);">
             <h2>${val.title}</h2>
             <p>${val.content}</p>
-            <p>By: ${val.author.firstName} ${val.author.lastName} on ${val.publishDate}
+            <p>By: ${val.author.firstName} ${val.author.lastName} on ${val.publishDate}</p>
             <p>post id: <i>${val._id}</i></p>
             </div>
+            <hr>
             `;
         })
         str += `</body>`;
         res.send(str)
     })
+    .catch(err => res.status(500).send("Internal server error"));
 });
 
 router.get('/:id', (req, res)=> {
     console.log('inside get');
     Post
     .find({_id: req.params.id})
-    .then((result) => res.json(result));
+    .then((result) => res.json(result))
+    .catch(err => res.status(500).send("Internal server error"));
+
 });
 
 router.post('/', jsonParser, (req,res)=>{
     //console.log('entered post thing');
     //console.log(req.body);
+  const requiredFields = ['title', 'content'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+
     Post.create(req.body).then((val)=>{
         //console.log("Yay!");
         res.status(201).json(val);
-    });
+    })
+    .catch(err => res.status(500).send("Internal server error"));
+
 });
 
 router.put('/:id', jsonParser, (req,res)=>{
     //console.log('entered post thing');
     //console.log(req.body);
+    if (req.params.id !== req.body.id){
+        res.status(400).send("body and parameter ID not equal!");
+    }
     Post.update({_id: req.params.id},{$set:req.body}).then((val)=>{
         //console.log("Yay!");
         res.status(201).json(val);
-    });
+    })
+    .catch(err => res.status(500).send("Internal server error"));
+
+    
 });
 
 router.delete('/:id',jsonParser,(req,res)=>{
-    Post.remove({_id: req.params.id}).then((val, err)=>{
-        res.json(val);
+    Post.remove({_id: req.params.id}).then((val)=>{
+        res.status(204).json(val);
     })
+    .catch(err => res.status(500).send("Internal server error"));
+
 });
 
 module.exports = router;
